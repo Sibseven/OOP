92955a289268dcaeebc4635388884f5dba7f292d
“Vadim_Lavrenenkov” <v.lavrenenkov@g.nsu.ru>
2024-09-10 15:26:55 +0700

    added tests and timers to check algorithm complexity

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -23,7 +23,11 @@ public class HeapsortTest {
     @Test
     void checkBasic() {
         int[] toSort = {5, 8, 9, 6, 7};
+        long startTime = System.currentTimeMillis();
         Heapsort.sort(toSort);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
         assertArrayEquals(toSort, new int[]{5, 6, 7, 8, 9});
     }
 
@@ -42,16 +46,96 @@ public class HeapsortTest {
     }
 
     @Test
-    void checkLarge() {
+    void checkLarge1_000_000() {
         int[] randomIntsArray = new int[1_000_000];
         Random random = new Random();
 
         for (int i = 0; i < 1_000_000; i++) {
             randomIntsArray[i] = random.nextInt(555555);
         }
-        
+
         int[]  randomIntsArray2 = randomIntsArray;
+        long startTime = System.currentTimeMillis();
         Heapsort.sort(randomIntsArray);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
+        Arrays.sort(randomIntsArray2);
+        assertArrayEquals(randomIntsArray, randomIntsArray2);
+    }
+
+    @Test
+    void checkLarge500_000() {
+        int[] randomIntsArray = new int[500_000];
+        Random random = new Random();
+
+        for (int i = 0; i < 500_000; i++) {
+            randomIntsArray[i] = random.nextInt(555555);
+        }
+
+        int[]  randomIntsArray2 = randomIntsArray;
+        long startTime = System.currentTimeMillis();
+        Heapsort.sort(randomIntsArray);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
+        Arrays.sort(randomIntsArray2);
+        assertArrayEquals(randomIntsArray, randomIntsArray2);
+    }
+
+    @Test
+    void checkLarge100000() {
+        int[] randomIntsArray = new int[1_000_00];
+        Random random = new Random();
+
+        for (int i = 0; i < 1_000_00; i++) {
+            randomIntsArray[i] = random.nextInt(555555);
+        }
+
+        int[]  randomIntsArray2 = randomIntsArray;
+        long startTime = System.currentTimeMillis();
+        Heapsort.sort(randomIntsArray);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
+        Arrays.sort(randomIntsArray2);
+        assertArrayEquals(randomIntsArray, randomIntsArray2);
+    }
+
+    @Test
+    void checkLarge10000() {
+        int[] randomIntsArray = new int[1_000_0];
+        Random random = new Random();
+
+        for (int i = 0; i < 1_000_0; i++) {
+            randomIntsArray[i] = random.nextInt(555555);
+        }
+
+        int[]  randomIntsArray2 = randomIntsArray;
+        long startTime = System.currentTimeMillis();
+        Heapsort.sort(randomIntsArray);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
+        Arrays.sort(randomIntsArray2);
+        assertArrayEquals(randomIntsArray, randomIntsArray2);
+    }
+
+    @Test
+    void checkLarge1000() {
+        int[] randomIntsArray = new int[1_000];
+        Random random = new Random();
+
+        for (int i = 0; i < 1_000; i++) {
+            randomIntsArray[i] = random.nextInt(555555);
+        }
+
+        int[]  randomIntsArray2 = randomIntsArray;
+        long startTime = System.currentTimeMillis();
+        Heapsort.sort(randomIntsArray);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Execution time:" + duration);
         Arrays.sort(randomIntsArray2);
         assertArrayEquals(randomIntsArray, randomIntsArray2);
     }
