2fd37c3a68d3a5164f0a9bdaa9d4ded4a8e01a2e
Sibseven <Sibseven@users.noreply.github.com>
2024-09-24 08:27:42 +0000

    Deploying to gh-pages from @ Sibseven/OOP@185050c0abfef96054f893c4107802e2f352c641 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nsu.lavrenenkov"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.lavrenenkov.blackjack"}]
