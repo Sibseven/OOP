2e8d4b56adb41e459084bfc715d87983f174c288
Vadim_Lavrenenkov <v.lavrenenkov@g.nsu.ru>
2024-10-22 15:55:35 +0700

    removed redundant code

--- Task_1_2_1/src/main/java/ru/nsu/lavrenenkov/graph/Edge.java
+++ Task_1_2_1/src/main/java/ru/nsu/lavrenenkov/graph/Edge.java
@@ -9,29 +9,4 @@ import java.util.Objects;
  */
 public record Edge<T>(Node<T> from, Node<T> to, int weight) {
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Edge<?> edge = (Edge<?>) o;
-        if (weight != edge.weight) {
-            return false;
-        }
-        if (!Objects.equals(from, edge.from)) {
-            return false;
-        }
-        return Objects.equals(to, edge.to);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = from != null ? from.hashCode() : 0;
-        result = 31 * result + (to != null ? to.hashCode() : 0);
-        result = 31 * result + weight;
-        return result;
-    }
 }
