e06dc7fd0a8568d8513e384bcd9db4cd3aad175b
Sibseven <Sibseven@users.noreply.github.com>
2024-12-03 09:08:12 +0000

    Deploying to gh-pages from @ Sibseven/OOP@269527c9e147529ad7b08d71ed247113e7e0d3d6 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"SubstringFinder"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.lavrenenkov.substring","l":"SubstringFinder"}];updateSearchResults();
