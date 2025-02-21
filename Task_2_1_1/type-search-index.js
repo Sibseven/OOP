0dd2f132570460cf20a6d778598fa53768f4d4dd
Sibseven <Sibseven@users.noreply.github.com>
2025-02-21 14:08:29 +0000

    Deploying to gh-pages from @ Sibseven/OOP@4a366dba8ff55dc2eb8ce2c6af78473bde426101 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.lavrenenkov.prime","l":"PrimeChecker"},{"p":"ru.nsu.lavrenenkov.prime","l":"PrimeThread"}];updateSearchResults();
